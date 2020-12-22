bf554a74e139e12e3ff819bca2c8d9018fb35d9b
Hamidreza Etebarian <oneHamidreza@gmail.com>
2020-12-23 01:07:54 +0330

    Some improvements & Bug fixes

--- app/src/main/kotlin/communere/ui/userIndex/UserAdapter.kt
+++ app/src/main/kotlin/communere/ui/userIndex/UserAdapter.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Hamidreza Etebarian & Ali Modares.
+ * Copyright (C) 2020 Hamidreza Etebarian.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
